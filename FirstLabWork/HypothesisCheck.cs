2875391e03f147d3c111f0d901c1d4bd6c7560cb
brother2000years <brthr2000@gmail.com>
2015-11-04 00:55:08 +0300

    Добавлены классы для проверки гипотез о виде закона распределения.

---
+++ FirstLabWork/HypothesisCheck.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstLabWork
+{
+    /// <summary>
+    /// Проверка гипотезы о виде закона распределения
+    /// </summary>
+    abstract class HypothesisCheck
+    {
+        /// <summary>
+        /// Выполнить проверку гипотезы
+        /// </summary>
+        /// <returns>Подтверждение гипотезы</returns>
+        abstract public bool doCheck();
+        /// <summary>
+        /// Рассчитать вероятность при значении inValue
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inValue">Аргумент для расчета вероятности</param>
+        /// <returns></returns>
+        abstract protected double probabilityFunction<T>(T inValue);
+    }
+
+    /// <summary>
+    /// Проверка гипотезы о нормальном законе распределения
+    /// </summary>
+    class NormalLawHypotesisCheck : HypothesisCheck 
+    {
+        public override bool doCheck()
+        {
+            throw new NotImplementedException();
+        }
+        protected override double probabilityFunction<T>(T inValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Проверка гипотезы о показательном законе распределения
+    /// </summary>
+    class ExponentialLawCheck : HypothesisCheck
+    {
+        public override bool doCheck()
+        {
+            throw new NotImplementedException();
+        }
+        protected override double probabilityFunction<T>(T inValue)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
